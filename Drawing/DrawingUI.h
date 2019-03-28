7f4bf6e1da5ae3a3ba845af7ca28d9a065dd1b08
Timothy White <timothy.white@eagles.oc.edu>
2019-03-27 21:07:56 -0500

    Efficiency update

--- Drawing/DrawingUI.h
+++ Drawing/DrawingUI.h
@@ -10,23 +10,21 @@ using namespace sf;
 class DrawingUI
 {
 private:
-	Vector2f pos;
+	RectangleShape canvas;
 public:
 	// Constructor
 	DrawingUI(Vector2f p)
 	{
-		pos = p;
+		canvas.setPosition(p);
+		canvas.setOutlineColor(Color::White);
+		canvas.setOutlineThickness(1);
+		canvas.setSize(Vector2f(550, 500));
 	}
 
 	// void draw(): Draws a box representing the canvas and all shapes in the ShapeMgr.
 	void draw(RenderWindow& win, ShapeMgr *mgr)
 	{
-		RectangleShape box;
-		box.setPosition(pos);
-		box.setOutlineColor(Color::White);
-		box.setOutlineThickness(1);
-		box.setSize(Vector2f(550, 500));
-		win.draw(box);
+		win.draw(canvas);
 
 		vector<DrawingShape*> shapes = mgr->getShapes();
 		for (int i = 0; i < shapes.size(); i++) {
@@ -37,10 +35,7 @@ public:
 	// bool isMouseInCanvas(): Returns a bool whether or not the mouse is in the canvas.
 	bool isMouseInCanvas(Vector2f mousePos)
 	{
-		return mousePos.x > pos.x &&
-			   mousePos.x < pos.x + 540 &&
-			   mousePos.y > pos.y &&
-			   mousePos.y < pos.y + 490;
+		return canvas.getGlobalBounds().contains(mousePos) && canvas.getGlobalBounds().contains(Vector2f(mousePos.x + 10, mousePos.y + 10));
 	}
 };
 
--- Drawing/SettingsUI.h
+++ Drawing/SettingsUI.h
@@ -15,32 +15,53 @@ class SettingsUI
 {
 private:
 	SettingsMgr *settings;
+	CircleShape red, green, blue;
+	CircleShape circle;
+	RectangleShape square;
 public:
 	// Constructor
 	SettingsUI(SettingsMgr *mgr)
 	{
 		settings = mgr;
+		red.setRadius(25);
+		red.setPosition(75, 100);
+		red.setOutlineColor(Color::Red);
+		red.setOutlineThickness(5);
+		green.setRadius(25);
+		green.setPosition(75, 175);
+		green.setOutlineColor(Color::Green);
+		green.setOutlineThickness(5);
+		blue.setRadius(25);
+		blue.setPosition(75, 250);
+		blue.setOutlineColor(Color::Blue);
+		blue.setOutlineThickness(5);
+		circle.setRadius(25);
+		circle.setPosition(75, 400);
+		circle.setOutlineColor(Color::White);
+		circle.setOutlineThickness(5);
+		square.setSize(Vector2f(50, 50));
+		square.setPosition(75, 475);
+		square.setOutlineColor(Color::White);
+		square.setOutlineThickness(5);
 	}
 
 	// void handleMouseUp(): Called when the mouse button is released. Checks if the mouse is on any of the buttons and changes the settings appropriately.
 	void handleMouseUp(Vector2f mouse)
 	{
-		if (mouse.x > 75 && mouse.x < 125) {
-			if (mouse.y > 100 && mouse.y < 150) {
-				settings->setColor(Color::Red);
-			}
-			else if (mouse.y > 175 && mouse.y < 225) {
-				settings->setColor(Color::Green);
-			}
-			else if (mouse.y > 250 && mouse.y < 300) {
-				settings->setColor(Color::Blue);
-			}
-			else if (mouse.y > 400 && mouse.y < 450) {
-				settings->setShape(CIRCLE);
-			}
-			else if (mouse.y > 475 && mouse.y < 525) {
-				settings->setShape(SQUARE);
-			}
+		if (red.getGlobalBounds().contains(mouse)) {
+			settings->setColor(Color::Red);
+		}
+		else if (green.getGlobalBounds().contains(mouse)) {
+			settings->setColor(Color::Green);
+		}
+		else if (blue.getGlobalBounds().contains(mouse)) {
+			settings->setColor(Color::Blue);
+		}
+		else if (circle.getGlobalBounds().contains(mouse)) {
+			settings->setShape(CIRCLE);
+		}
+		else if (square.getGlobalBounds().contains(mouse)) {
+			settings->setShape(SQUARE);
 		}
 	}
 
@@ -57,18 +78,6 @@ public:
 		colorLabel.setPosition(15, 50);
 		colorLabel.setFont(font);
 		win.draw(colorLabel);
-
-		CircleShape red(25), green(25), blue(25);
-		red.setPosition(75, 100);
-		green.setPosition(75, 175);
-		blue.setPosition(75, 250);
-
-		red.setOutlineColor(Color::Red);
-		green.setOutlineColor(Color::Green);
-		blue.setOutlineColor(Color::Blue);
-		red.setOutlineThickness(5);
-		green.setOutlineThickness(5);
-		blue.setOutlineThickness(5);
 
 		red.setFillColor(Color::Transparent);
 		green.setFillColor(Color::Transparent);
@@ -93,14 +102,6 @@ public:
 		shapeLabel.setFont(font);
 		win.draw(shapeLabel);
 
-		CircleShape circle(25);
-		RectangleShape square(Vector2f(50, 50));
-		circle.setPosition(75, 400);
-		square.setPosition(75, 475);
-		circle.setOutlineColor(Color::White);
-		square.setOutlineColor(Color::White);
-		circle.setOutlineThickness(5);
-		square.setOutlineThickness(5);
 		circle.setFillColor(Color::Transparent);
 		square.setFillColor(Color::Transparent);
 		ShapeEnum s = settings->getCurShape();
